e8baff0d1337f90b5834e8bab0c3b9e3ae4d840f
dongwook98 <ehddnr8563@gmail.com>
2023-11-01 02:09:37 +0900

    웹팩 설정 추가

--- 1.구구단/webpack.config.js
+++ 1.구구단/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         presets: [
           ['@babel/preset-env', {
             targets: {
-              browsers: ['> 1% in KR  '], // browserslist
+              browsers: ['> 1% in KR'], // browserslist
               // 원하는 브라우저에만 babel-preset-env를 적용시키게 해줄수있음. (모든 브라우저에 적용하면 바벨작업량이 많아져서 느려질수도)
             },
             debug: true,
